ec0cdbbcd916d8646df9a325b3b5c745425c869a
Riski Nugroho Putra <31657885+riskinputra@users.noreply.github.com>
2022-10-05 17:24:30 +0700

    first init

---
+++ src/views/Dashboard/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="mt-10 text-3xl mx-auto max-w-6xl">
+    Dashboard index {{ store }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Dashboard',
+  computed: {
+    store() {
+      return window?.localStorage?.test ?? ''
+    }
+  }
+}
+</script>
